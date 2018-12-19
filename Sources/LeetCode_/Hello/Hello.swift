36171a0b8a23dfe5d1c32d1775b7bee22e17d430
yuanxuhan <yuanxuhan@tencent.com>
2018-12-19 13:00:31 +0800

    init & add hello world

---
+++ Sources/LeetCode_/Hello/Hello.swift
@@ -0,0 +1,16 @@
+//
+//  Hello.swift
+//  LeetCode_
+//
+//  Created by 韩元旭 on 2018/12/19.
+//
+
+import Foundation
+
+extension Solution {
+    
+    func hello() {
+        print("Hello Stermop")
+    }
+    
+}
